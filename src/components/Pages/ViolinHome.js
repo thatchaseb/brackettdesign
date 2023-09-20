14bd0bf804f5e84192cde1d3241144a194a2750f
thatchaseb <77036934+thatchaseb@users.noreply.github.com>
2023-09-20 14:15:50 -0400

    Create ViolinHome.js

---
+++ src/components/Pages/ViolinHome.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Element } from 'react-scroll'
+
+const ViolinHome = () => {
+  
+    
+
+  return (
+    <Element name="ViolinHome" class="">
+      <div class="">
+        
+        Violin Home
+        
+      </div>
+    
+    </Element>
+
+  );
+};
+
+export default ViolinHome;
